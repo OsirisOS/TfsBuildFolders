0cccda9d401fa85d3a2dda65c7bcb34720e3a1c2
terje2000_cp <SND\terje2000_cp>
2011-11-25 10:24:27 +0100

    Added missing files

---
+++ main/src/Inmeta.VisualStudio.TeamExplorer.Package/ToolsOptions/BuildExplorerSettings.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Inmeta.VisualStudio.TeamExplorer
+{
+    [Serializable]
+    public class BuildExplorerSettings
+    {
+        public enum QueueDefaultBuildControlValues
+        {
+            DontAllow = 0,
+            DontAllowOnFolder = 1,
+            Allow = 2
+        }
+
+
+        public BuildExplorerSettings()
+        {
+            Separator = ".";
+            QueueDefaultBuildControl = QueueDefaultBuildControlValues.Allow;
+            //UseTimedRefreshAtStartup = true;
+            //TimerDelay = 3000;
+        }
+        [XmlElement("Separator")]
+        public string Separator { get; set; }
+
+        
+        
+        [XmlElement("QueueDefaultBuildControl")]
+        public QueueDefaultBuildControlValues QueueDefaultBuildControl { get; set; }
+
+        // [XmlElement("UseTimedRefreshAtStartup")]
+        //public bool UseTimedRefreshAtStartup { get; set; }
+
+        ///// <summary>
+        ///// Delay is in mS
+        ///// </summary>
+        //[XmlElement("TimedRefreshDelay")]
+        //public int TimerDelay { get; set; }
+    
+    }
+}
